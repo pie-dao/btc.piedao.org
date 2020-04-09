96d3cf22c8c31eeebc6589e084f70c5fe0ac87d0
Dan Matthews <dan@cryptobuilders.io>
2020-04-09 12:53:28 +0300

    Add account conditional wrapping around Liquidity page

--- src/pages/Liquidity.jsx
+++ src/pages/Liquidity.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { eth } from '@pie-dao/eth';
 import { Mint } from '@pie-dao/mint';
 import { view } from '@risingstack/react-easy-state';
 
@@ -10,12 +11,24 @@ const handleClick = () => {
     + 'outputCurrency=0x0327112423F3A68efdF1fcF402F6c5CB9f7C33fd';
 };
 
-const Liquidity = (props) => (
-  <div className="liquidity-container content">
-    <Mint {...props} />
-    <PoolBalance {...props} />
-    <button className="btn" type="button" onClick={handleClick}>or get it on Uniswap</button>
-  </div>
-);
+const Liquidity = (props) => {
+  if (eth.account) {
+    return (
+      <div className="liquidity-container content">
+        <Mint {...props} />
+        <PoolBalance {...props} />
+        <button className="btn" type="button" onClick={handleClick}>
+          or get BTC++ on Uniswap
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="liquidity-container content">
+      <button className="btn" type="button" onClick={handleClick}>Get BTC++ on Uniswap</button>
+    </div>
+  );
+};
 
 export default view(Liquidity);
