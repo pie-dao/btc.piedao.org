be369dfde5e6f6641838d379f89d1bf0c779385a
Dan Matthews <dan@cryptobuilders.io>
2020-04-09 12:44:55 +0300

    add uniswap button

--- src/pages/Liquidity.jsx
+++ src/pages/Liquidity.jsx
@@ -5,10 +5,16 @@ import { view } from '@risingstack/react-easy-state';
 
 import PoolBalance from '../components/PoolBalance';
 
+const handleClick = () => {
+  window.location.href = 'https://uniswap.exchange/swap?'
+    + 'outputCurrency=0x0327112423F3A68efdF1fcF402F6c5CB9f7C33fd';
+};
+
 const Liquidity = (props) => (
   <div className="liquidity-container content">
     <Mint {...props} />
     <PoolBalance {...props} />
+    <button className="btn" type="button" onClick={handleClick}>or get it on Uniswap</button>
   </div>
 );
 
