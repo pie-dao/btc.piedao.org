63d8900585ae7b0c36e67af13f88988e8a235bd1
Nicola Di Marco <dimarco.nicola@gmail.com>
2020-04-16 18:00:14 +0200

    Add Balancer Button

--- src/pages/Liquidity.jsx
+++ src/pages/Liquidity.jsx
@@ -11,6 +11,10 @@ const handleClick = () => {
     + 'outputCurrency=0x0327112423F3A68efdF1fcF402F6c5CB9f7C33fd';
 };
 
+const handleClickBalancer = () => {
+  window.location.href = 'https://balancer.exchange/#/swap/ether/0x0327112423F3A68efdF1fcF402F6c5CB9f7C33fd';
+};
+
 const Liquidity = (props) => {
   if (eth.account) {
     return (
@@ -20,6 +24,9 @@ const Liquidity = (props) => {
         <button className="btn" type="button" onClick={handleClick}>
           or get BTC++ on Uniswap
         </button>
+        <button className="btn" type="button" onClick={handleClickBalancer}>
+          or get BTC++ on Balancer
+        </button>
       </div>
     );
   }
@@ -27,7 +34,9 @@ const Liquidity = (props) => {
   return (
     <div className="liquidity-container content">
       <button className="btn" type="button" onClick={handleClick}>Get BTC++ on Uniswap</button>
+      <button className="btn" type="button" onClick={handleClickBalancer}>Get BTC++ on Balancer</button>
     </div>
+
   );
 };
 
